b6979c0cd15ea31e296175854f08465655dc3825
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-06-11 22:08:47 +0200

    filter

--- webshop/public/index.php
+++ webshop/public/index.php
@@ -33,7 +33,7 @@ include_once "assets/php/classes.php";
     <nav class="topNav">
         <ul class="topNav__list">
             <li class="list__item">
-                <a class="navItem__link--Home" href="index.php?Home=true">
+                <a class="navItem__link--Home" href="index.php?Home=Welkom">
                     <img class="navItem__logo" src="" alt="Logo">
                 </a>
                 <a class="navItem__link--shopping_card" href="">
@@ -62,40 +62,69 @@ include_once "assets/php/classes.php";
     </section>
     <div class="div__intersection"></div>
     <div class="div__search-dropdown">
-        <input type="search" name="" id="">
+        <input type="search" name="" id="searchbar">
         <div class="dropdown">
             <button class="dropbtn">Filter</button>
             <div class="dropdown-content">
                 <a href="index.php?dark_blue=true">dark blue</a>
-                <a href="#">green</a>
-                <a href="#">light blue</a>
-                <a href="#">orange</a>
-                <a href="#">pink</a>
-                <a href="#">black</a>
+                <a href="index.php?green_usb=true">green</a>
+                <a href="index.php?licht_blue_usb=true">light blue</a>
+                <a href="index.php?orange_usb=true">orange</a>
+                <a href="index.php?pink_usb=true">pink</a>
+                <a href="index.php?black_usb=true">black</a>
             </div>
         </div>
     </div>
-    <div class="div__products">
+    <div class="div__seudo-products">
         <?php
 
-        // function first_product() {
-
-        //     print("<div class=\"products__card\">");
-        //     print("    <img id=\"dark_blue_usb\" class=\"products-card__img\" src=\"$dark_blue_usb->image\" alt=\"placeholder\">");
-        //     print("    <h1 class=\"products-card__product-name\">$dark_blue_usb->product_name</h1>");
-        //     print("    <h2 class=\"products-card__product-price\">$dataObject->product_price</h2>");
-        //     print("    <button class=\"products-card__button\">Add to card</button>");
-        //     print("</div>"); 
-        // }
-
+// IF statement about the filter button
         if (isset($_GET['dark_blue'])) {
             print("<div class=\"products__card\">");
-            print("    <img id=\"dark_blue_usb\" class=\"products-card__img\" src=\"$dark_blue_usb->image\" alt=\"placeholder\">");
+            print("    <img id=\"Usb-Dark-Blue\" class=\"products-card__img\" src=\"$dark_blue_usb->image\" alt=\"placeholder\">");
             print("    <h1 class=\"products-card__product-name\">$dark_blue_usb->product_name</h1>");
             print("    <h2 class=\"products-card__product-price\">$dark_blue_usb->product_price</h2>");
             print("    <button class=\"products-card__button\">Add to card</button>");
             print("</div>"); 
+        } elseif (isset($_GET['green_usb'])) {
+            print("<div class=\"products__card\">");
+            print("    <img id=\"Usb-Green\" class=\"products-card__img\" src=\"$green_usb->image\" alt=\"placeholder\">");
+            print("    <h1 class=\"products-card__product-name\">$green_usb->product_name</h1>");
+            print("    <h2 class=\"products-card__product-price\">$green_usb->product_price</h2>");
+            print("    <button class=\"products-card__button\">Add to card</button>");
+            print("</div>"); 
+        } elseif (isset($_GET['licht_blue_usb'])) {
+            print("<div class=\"products__card\">");
+            print("    <img id=\"Usb-Licht-Blue\" class=\"products-card__img\" src=\"$licht_blue_usb->image\" alt=\"placeholder\">");
+            print("    <h1 class=\"products-card__product-name\">$licht_blue_usb->product_name</h1>");
+            print("    <h2 class=\"products-card__product-price\">$licht_blue_usb->product_price</h2>");
+            print("    <button class=\"products-card__button\">Add to card</button>");
+            print("</div>"); 
+        } elseif (isset($_GET['orange_usb'])) {
+            print("<div class=\"products__card\">");
+            print("    <img id=\"Usb-Orange\" class=\"products-card__img\" src=\"$orange_usb->image\" alt=\"placeholder\">");
+            print("    <h1 class=\"products-card__product-name\">$orange_usb->product_name</h1>");
+            print("    <h2 class=\"products-card__product-price\">$orange_usb->product_price</h2>");
+            print("    <button class=\"products-card__button\">Add to card</button>");
+            print("</div>"); 
+        } elseif (isset($_GET['pink_usb'])) {
+            print("<div class=\"products__card\">");
+            print("    <img id=\"Usb-Pink\" class=\"products-card__img\" src=\"$pink_usb->image\" alt=\"placeholder\">");
+            print("    <h1 class=\"products-card__product-name\">$pink_usb->product_name</h1>");
+            print("    <h2 class=\"products-card__product-price\">$pink_usb->product_price</h2>");
+            print("    <button class=\"products-card__button\">Add to card</button>");
+            print("</div>"); 
+        } elseif (isset($_GET['black_usb'])) {
+            print("<div class=\"products__card\">");
+            print("    <img id=\"Usb-Black\" class=\"products-card__img\" src=\"$black_usb->image\" alt=\"placeholder\">");
+            print("    <h1 class=\"products-card__product-name\">$black_usb->product_name</h1>");
+            print("    <h2 class=\"products-card__product-price\">$black_usb->product_price</h2>");
+            print("    <button class=\"products-card__button\">Add to card</button>");
+            print("</div>"); 
         } else {
+
+            print("</div>");
+            print("<div class=\"div__products\">");
 
             foreach ($dataObject as $items) {
                 asort($dataObject);
